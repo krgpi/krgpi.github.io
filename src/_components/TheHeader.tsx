720389135376b3dc9b2f5c23dc804f928368a4bb
Karaage <karaage01@outlook.jp>
2023-12-12 11:21:46 +0900

    apply custom color to the header

--- src/_components/TheHeader.tsx
+++ src/_components/TheHeader.tsx
@@ -5,16 +5,19 @@ const TheHeader: React.FC = () => {
     <div className="text-center font-raj">
       <div id="header" className="bg-krg-bg-slate">
         <h1 className="text-white text-3xl py-2">
-          <a href="/">Portfolio</a>
+          <a href="/">Karaage Project</a>
         </h1>
-        <a className="text-orange-600 hover:underline px-4" href="/">
+        <a className="text-krg-link-orange hover:underline px-4" href="/">
           Home
         </a>
-        <a className="text-orange-600 hover:underline px-4" href="/works">
+        <a className="text-krg-link-orange hover:underline px-4" href="/works">
           Works
         </a>
-        <a className="text-orange-600 hover:underline px-4" href="/activities">
-          Activities
+        <a
+          className="text-krg-link-orange hover:underline px-4"
+          href="/activities"
+        >
+          Links
         </a>
       </div>
     </div>
