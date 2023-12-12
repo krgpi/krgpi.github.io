40162e1ec173ddc2a13f84d86b41170f119ede09
Karaage <karaage01@outlook.jp>
2023-12-12 11:21:15 +0900

    add lighter icon image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,23 +2,24 @@ import React from "react";
 
 const IndexPage: React.FC = () => {
   return (
-    <div className="bg-krg-bg-gray text-white p-1">
-      <div>
+    <div className="bg-krg-bg-gray text-white p-4">
+      <p className="text-xl">Profile</p>
+      <div className="max-w-lg rounded overflow-hidden border p-4">
         <div className="flex flex-col md:flex-row space-x-4">
           <div>
             <img
               className="rounded-full"
               width="100"
-              src="https://www.ikaraage.com/icons/img/krgpi_icon_illust.png"
+              src="src/assets/icon.png"
               alt="icon"
             />
           </div>
           <div>
             <div>
-              <h1>
-                <b>Yuki NOAKI</b>
-              </h1>
-              <h4>a.k.a. Karaage, Software Engineer at Tokyo, Japan</h4>
+              <p className="text-2xl">Yuki NOAKI</p>
+              <p className="text-large">
+                a.k.a. Karaage, Software Engineer at Tokyo, Japan
+              </p>
             </div>
             <p>
               スタートアップの立ち上げや成長を、エンジニアリングとプロダクトマネジメントで支える仕事をしている。
