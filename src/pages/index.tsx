e712997f0fd847e70474e23d8e2369715acff910
Karaage <karaage01@outlook.jp>
2023-12-12 13:00:16 +0900

    add release page of cloud stickies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,21 @@ import React from "react";
 const IndexPage: React.FC = () => {
   return (
     <div className="bg-krg-bg-gray text-white p-4">
-      <p className="text-xl">Profile</p>
+      <p className="text-3xl">Featured</p>
+      <div className="max-w-lg rounded overflow-hidden border p-4">
+        <p className="text-medium">Dec 12, 2023</p>
+        <a
+          className="text-2xl font-bold hover:underline text-krg-link-orange"
+          href="releases/cloudstickies"
+        >
+          The Stickies app, which syncs with iCloud, has been released.
+        </a>
+        <p className="text-large">The app for macOS is available.</p>
+        <p className="text-large">
+          The app for iOS will be available on Feb 2024.
+        </p>
+      </div>
+      <p className="text-3xl">Profile</p>
       <div className="max-w-lg rounded overflow-hidden border p-4">
         <div className="flex flex-col md:flex-row space-x-4">
           <div>
@@ -20,9 +34,13 @@ const IndexPage: React.FC = () => {
               <p className="text-large">
                 a.k.a. Karaage, Software Engineer at Tokyo, Japan
               </p>
+              <p className="text-large font-bold">
+                Product Manager & Scrum Master @ CoeFont Co, Ltd.
+              </p>
             </div>
             <p>
-              スタートアップの立ち上げや成長を、エンジニアリングとプロダクトマネジメントで支える仕事をしている。
+              I work to support the launch and growth of startups with
+              engineering and product management.
             </p>
             <p>hobbies: 📸 🎸 🚘</p>
           </div>
---
+++ src/pages/releases.cloudstickies.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+const IndexPage: React.FC = () => {
+  return (
+    <div className="bg-krg-bg-gray text-white p-4">
+      {" "}
+      <div className="main_body">
+        <div className="body_container">
+          <div className="space-y-4">
+            For creators, developers, and everyone who build future, I provide
+            powerful tools make your work efficient extremely.
+            <p className="text-3xl">macOS App</p>
+            <div className="space-y-2">
+              <p className="text-2xl">Cloud Stickies</p>
+              <p>iCloudで同期できる付箋アプリです。</p>
+              <p>
+                <b>
+                  <a href="/releases/cloudstickies/CloudStickies-1.0.5.zip">
+                    ダウンロード / Download
+                  </a>
+                </b>
+              </p>
+              <p className="text-xl font-bold">特徴</p>
+              <ul>
+                <li>
+                  付箋を整理するウィンドウがあり、これはメニューバーから表示することもできます
+                </li>
+                <li>
+                  ピン留め機能：ピン留めした付箋は他のアプリ使用中も常に最前面に表示されます
+                </li>
+                <li>付箋の色を設定でき、これは端末間で同期されます</li>
+              </ul>
+              <hr />
+              <p>You can sync sticky note via iCloud.</p>
+              <p className="text-xl font-bold">Features</p>
+              <ul>
+                <li>
+                  It has an organize window, that you can choose notes which you
+                  would like to display. You can also display this window from
+                  menubar.
+                </li>
+                <li>
+                  Note can be pinned. Pinned note is always display front even
+                  if you would select other app window.
+                </li>
+                <li>
+                  You can set color of note, and it also be synchronized via
+                  iCloud.
+                </li>
+              </ul>
+            </div>
+            <p className="text-3xl">iOS App</p>
+            <div className="category-box">
+              <p className="text-2xl">Cloud Stickies iOS</p>
+              <p>You can sync sticky note via iCloud.</p>
+              <p>
+                <i>Coming soon... maybe I can release it by Feb 2024.</i>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default IndexPage;
