39fa6adf6085154b1c36a3d033ec71409ae775e3
krgpi <17940589+krgpi@users.noreply.github.com>
2023-12-13 09:41:14 +0900

    github pagesへのデプロイ設定を新規作成

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,4 @@ import generouted from '@generouted/react-router/plugin'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-export default defineConfig({ plugins: [react(), generouted()] })
+export default defineConfig({ plugins: [react(), generouted()] , base: '/'})
