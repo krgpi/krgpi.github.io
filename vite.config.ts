3314bce6a6fcce8e542cc795dd978d326cce9ef9
Karaage <karaage01@outlook.jp>
2024-06-04 13:17:07 +0900

    カスタム404ページへのエントリーポイントを作成

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,15 @@
-import generouted from '@generouted/react-router/plugin'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import generouted from '@generouted/react-router/plugin';
+import react from '@vitejs/plugin-react';
+import { resolve } from "path";
+import { defineConfig } from 'vite';
 
-export default defineConfig({ plugins: [react(), generouted()] })
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                404: resolve(__dirname, '404.html'),
+            }
+        }
+    }, plugins: [react(), generouted()]
+})
