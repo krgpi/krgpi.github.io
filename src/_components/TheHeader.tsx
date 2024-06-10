7b5f11876e1260ae8df16cc1c74465b6f77c4600
Karaage <karaage01@outlook.jp>
2024-06-10 18:19:36 +0900

    タイトル変更

--- src/_components/TheHeader.tsx
+++ src/_components/TheHeader.tsx
@@ -6,7 +6,7 @@ const TheHeader: React.FC = () => {
     <div className="text-center font-raj">
       <div id="header" className="bg-krg-bg-slate">
         <h1 className="text-white text-3xl py-2">
-          <Link to="/">Karaage Project</Link>
+          <Link to="/">Karaage's Library</Link>
         </h1>
         <Link className="text-krg-link-orange hover:underline px-4" to="/">
           Home
